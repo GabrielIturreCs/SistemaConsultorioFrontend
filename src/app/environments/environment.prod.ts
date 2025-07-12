15cd135730188fcd4a99405901af9ad8cb7aea72
GabrielIturreCs <166109298+GabrielIturreCs@users.noreply.github.com>
2025-07-12 05:16:16 -0300

    Actualizar URL de la API en el entorno de producción: se cambió la dirección de la API a 'https://sistemaconsultoriobackend.onrender.com/api' para reflejar la nueva configuración del backend.

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://registro-de-turno-sistema-odontologico.onrender.com/api', // ✅ URL correcta del backend
+  apiUrl: 'https://sistemaconsultoriobackend.onrender.com/api', // ✅ URL correcta del backend
   googleClientId: "585856608795-5tnd7elkktntqm7b95g4livkppcjgp1u.apps.googleusercontent.com",
   someFeatureToggle: true
 };
