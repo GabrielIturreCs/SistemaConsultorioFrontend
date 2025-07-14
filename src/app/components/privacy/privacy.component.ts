48b36e5b84200c8d7fdd6c848ef144b5c6080462
GabrielIturreCs <166109298+GabrielIturreCs@users.noreply.github.com>
2025-07-13 22:14:05 -0300

    Actualizar el componente de Política de Privacidad y Términos de Servicio: se corrigió la forma de mostrar el correo electrónico para evitar el spam y se añadió el soporte para DatePipe, mejorando la funcionalidad y presentación de ambos componentes.

--- src/app/components/privacy/privacy.component.ts
+++ src/app/components/privacy/privacy.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-privacy',
   templateUrl: './privacy.component.html',
-  styleUrls: ['./privacy.component.css']
+  styleUrls: ['./privacy.component.css'],
+  imports: [DatePipe],
+  standalone: true
 })
 export class PrivacyComponent {
   fechaActual = new Date();
--- src/app/components/terms/terms.component.ts
+++ src/app/components/terms/terms.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-terms',
   templateUrl: './terms.component.html',
-  styleUrls: ['./terms.component.css']
+  styleUrls: ['./terms.component.css'],
+  imports: [DatePipe],
+  standalone: true
 })
 export class TermsComponent {
   fechaActual = new Date();
